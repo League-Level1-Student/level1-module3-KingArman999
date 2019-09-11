42517e84d9bbc52cacb34fcbf61d07d574463cda
League Student <league@iMac-14.attlocal.net>
2019-09-10 19:47:00 -0700

    Neflix and and more

--- src/_05_netflix/Netflix.java
+++ src/_05_netflix/Netflix.java
@@ -2,10 +2,25 @@ package _05_netflix;
 
 public class Netflix {
 public static void main(String[] args) {
-	Movie fort = new Movie("Fortnite", 0);
-	Movie mine = new Movie("Minecraft:3D",10);
+	Movie fort = new Movie("Fortnite Twilight", 0);
+	Movie mine = new Movie("Minecraft",10);
 	Movie john = new Movie("John Wick:4",3);
-	Movie bois = new Movie("Me and the Boys",2);
+	Movie shrek = new Movie("Shrek 5",69);
 	Movie blox = new Movie("Roblox",1);
+	blox.getTicketPrice();
+	fort.getTicketPrice();
+	mine.getTicketPrice();
+	john.getTicketPrice();
+	shrek.getTicketPrice();
+	NetflixQueue q = new NetflixQueue();
+	q.addMovie(blox);
+	q.addMovie(fort);
+	q.addMovie(mine);
+	q.addMovie(john);
+	q.addMovie(shrek);
+	q.printMovies();
+	System.out.println("The best movie is " + q.getBestMovie());
+	q.sortMoviesByRating();
+	System.out.println("The second best movies is "+q.getMovie(1));
 }
 }
